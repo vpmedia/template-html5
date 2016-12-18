90dfb0eb8374d93a55907524c0361d0bfab226a1
vpmedia <andras@vpmedia.eu>
2016-12-18 09:44:44 +0100

    Improve dev env.

--- src/main/client/app.jsx
+++ src/main/client/app.jsx
@@ -27,7 +27,7 @@ App.propTypes = {
 // initialize React
 const main = new Main('HTML5 Phaser / React example using ES6');
 const appMessage = main.init();
-//document.querySelector('.app').innerText = appMessage;
+// document.querySelector('.app').innerText = appMessage;
 ReactDOM.render(<App message={appMessage} />, document.querySelector('.app'));
 // initialize Phaser
 window.game = new Game();
